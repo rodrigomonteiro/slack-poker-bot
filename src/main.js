20a1bb571b4fb210a7e3b7a67260349b6870fc42
Rodrigo Monteiro do Amaral <montanha@montanha-2.local>
2015-10-18 17:36:59 -0200

    teste montodb 2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 require('babel/register');
 var http = require('http');
-var mongo = require('mongodb');
+//var mongo = require('mongodb');
 
 try {
   var fs = require('fs');
@@ -17,11 +17,33 @@ bot.login();
 
 // Heroku requires the process to bind to this port within 60 seconds or it is killed 
 http.createServer(function(req, res) {
-  mongo.Db.connect('mongodb://teste:teste@ds041144.mongolab.com:41144/heroku_lxn509sn', function (err, db) {
-    db.collection('test_init', function(er, collection) {
-      var dateNow = new Date()
-      collection.insert({'hora': dateNow}, {safe: true}, function(er,rs) {
-      });
+  
+  //mongo.Db.connect('mongodb://teste:teste@ds041144.mongolab.com:41144/heroku_lxn509sn', function (err, db) {
+  //  db.collection('test_init', function(er, collection) {
+  //    var dateNow = new Date()
+  //    collection.insert({'hora': dateNow}, {safe: true}, function(er,rs) {
+  //    });
+  //  });
+  //});
+
+  var MongoClient = require('mongodb').MongoClient;
+  //var ObjectId = require('mongodb').ObjectID;
+  var url = 'mongodb://ds041154.mongolab.com:41154/heroku_q2m8chsx';
+
+  var insertDocument = function(db, callback) {
+    db.collection('test_init').insertOne( {
+      "acesso" : new Date()
+    }, function(err, result) {
+      assert.equal(err, null);
+      console.log("Inserted a document into the test_init collection.");
+      callback(result);
+    });
+  };
+
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    insertDocument(db, function() {
+      db.close();
     });
   });
 
